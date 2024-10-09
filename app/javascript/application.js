a6382275bb266aaa32b3c1a17cac50be9e3504a7
sady paul <sd.marcoleta@hotmail.com>
2024-10-08 22:44:50 -0300

    para subir a github

---
+++ app/javascript/application.js
@@ -0,0 +1,9 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+
+import "./jquery.min"         // Ajustado para usar el prefijo './'
+import "./browser.min"        // Ajustado para usar el prefijo './'
+import "./breakpoints.min"    // Ajustado para usar el prefijo './'
+import "./main"               // Ajustado para usar el prefijo './'
